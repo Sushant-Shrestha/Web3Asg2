758aa6fe1bd6fb8f9b1275a6ce4427b6ee4a9ddb
Sushant-Stha <sshre733@mtroyal.ca>
2020-02-25 01:15:17 -0700

    Adding animation to detail view

--- my-app/src/components/MovieDetails.js
+++ my-app/src/components/MovieDetails.js
@@ -7,6 +7,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar, faStarHalfAlt } from '@fortawesome/free-solid-svg-icons';
 import { faStar as faEmptyStar } from '@fortawesome/free-regular-svg-icons';
 import * as cloneDeep from 'lodash/cloneDeep';
+import DetailsAnimation from '../animation/DetailsAnimation';
 
 class MovieDetails extends React.Component {
     constructor(props) {
@@ -27,7 +28,8 @@ class MovieDetails extends React.Component {
             colourImage: "#fff",
             colourText: "#bbbb",
             avg: '',
-            starArray: []
+            starArray: [],
+            detailAnim: true
         }
     }
 
@@ -63,6 +65,9 @@ class MovieDetails extends React.Component {
             
 
             this.setState({starArray: tempArray});
+            setTimeout(() => {
+                this.setState({detailAnim: false})
+            }, 2000);
         } catch (error) {
             console.log(error);
         }
@@ -109,100 +114,104 @@ class MovieDetails extends React.Component {
     render() {
         return (
             <div>
-                <FontAwesomeIcon icon={faStar} />
-                <FontAwesomeIcon icon={faStarHalfAlt} />
-                <FontAwesomeIcon icon={faEmptyStar} />
-                {this.state.viewingCast ? (<div>
-
-                    <Cast id={this.state.castID} cast={this.state.cast} crew={this.state.crew} closeView={this.closeView} setViewCast={this.setViewCast} updateViewCast={this.updateViewCast} />
-                </div>)
-                    : (
-                        <div className='mainView' style={{ display: 'grid', gridTemplateColumns: '1fr 2fr  ', gridColumn: 'span 1' }}>
-
-
-
-                            <LeftMovieDetails className='subView'> <div className="movieDetails subView" style={{ backgroundColor: this.state.colourImage, boxShadow: 'none' }}>
-                                <h2 style={{ color: this.state.colourText }}>{this.state.movie.title}</h2>
-                                <img src={"https://image.tmdb.org/t/p/w342/" + this.state.movie.poster} />
+                {this.state.detailAnim ? (<DetailsAnimation />) : (
+                    <div>
+                    <FontAwesomeIcon icon={faStar} />
+                    <FontAwesomeIcon icon={faStarHalfAlt} />
+                    <FontAwesomeIcon icon={faEmptyStar} />
+                    {this.state.viewingCast ? (<div>
+    
+                        <Cast id={this.state.castID} cast={this.state.cast} crew={this.state.crew} closeView={this.closeView} setViewCast={this.setViewCast} updateViewCast={this.updateViewCast} />
+                    </div>)
+                        : (
+                            <div className='mainView' style={{ display: 'grid', gridTemplateColumns: '1fr 2fr  ', gridColumn: 'span 1' }}>
+    
+    
+    
+                                <LeftMovieDetails className='subView'> <div className="movieDetails subView" style={{ backgroundColor: this.state.colourImage, boxShadow: 'none' }}>
+                                    <h2 style={{ color: this.state.colourText }}>{this.state.movie.title}</h2>
+                                    <img src={"https://image.tmdb.org/t/p/w342/" + this.state.movie.poster} />
+                                </div>
+                                </LeftMovieDetails>
+    
+                                <RightMovieDetails className='subView'>
+                                    <button className='fa fa-close' onClick={this.normalView} style={{ float: 'right' }}></button>
+    
+                                    <div>
+                                        <BoxDetails> Release date - {this.state.movie.release_date} <br /></BoxDetails>
+    
+                                        <BoxDetails>Revenue - ${this.state.movie.revenue} <br /></BoxDetails>
+                                        <BoxDetails> Runtime - {this.state.movie.runtime}m <br /></BoxDetails>
+                                        <BoxDetails> Tagling - {this.state.movie.tagline} <br /></BoxDetails>
+                                        <BoxDetails> <a href={"https://www.themoviedb.org/movie/" + this.state.movie.tmdb_id}> TMDB LINK</a> <br /></BoxDetails>
+                                        <BoxDetails> <a href={"https://www.imdb.com/title/" + this.state.movie.imdb_id}>IMDB LINK</a> <br /> </BoxDetails>
+                                        <BoxDetails> Overview -{this.state.overview} <br /> </BoxDetails>
+                                        <BoxDetails> Popularity-{this.state.ratings.popularity} <br /></BoxDetails>
+    
+    
+                                        <BoxDetails> Average- {this.state.ratings.average} <br />
+                                            {/* <StarRatings avg={this.state.avg}/> */}
+                                            {this.state.starArray.map((s, i) => s)}
+    
+                                        </BoxDetails>
+                                        <BoxDetails> Count-{this.state.ratings.count} <br /></BoxDetails>
+    
+                                        <BoxDetails>Companies - {this.state.companies ? (<div> {this.state.companies.map((c, index) => {
+                                            return c.name
+                                        })} </div>) : (<div>
+                                            Companies are not available
+                                        </div>)}</BoxDetails>
+    
+                                        <BoxDetails>Countries - {this.state.countries ? (<div> {this.state.countries.map((c, index) => {
+                                            return c.name
+                                        })} </div>) : (<div>
+                                            Countries are not available
+                                        </div>)}</BoxDetails>
+                                        {/* Countries - {this.state.countries.map((c, index) => {
+                                            return c.name
+                                        })} <br /> */}
+    
+                                        <BoxDetails> <h3>Keywords</h3> <br/> {this.state.keywords ? (<div> {this.state.keywords.map((c, index) => {
+                                            return c.name + " "
+                                        })} </div>) : (<div>
+                                            Keywords are not available
+                                        </div>)}</BoxDetails>
+                                        {/* Keywords - {this.state.keywords.map((c, index) => {
+                                            return c.name
+                                        })} <br /> */}
+    
+                                        <BoxDetails>Genres - {this.state.genres ? (<div> {this.state.genres.map((c, index) => {
+                                            return c.name + " "
+                                        })} </div>) : (<div>
+                                            Genres are not available
+                                        </div>)}</BoxDetails>
+    
+                                        {/* Genres - {this.state.genres.map((c, index) => {
+                                            return ", " + c.name 
+                                        })} <br /> */}
+                                    </div>
+    
+                                </RightMovieDetails>
+    
+                                <ProductionList className='subView'>
+                                    <div>
+                                        <h2> Production</h2>
+                                        <u>Cast</u> <br />
+                                        {this.state.cast ? (<div> {this.state.cast.map((c, index) => {
+                                            return <Production key={index} setViewCast={this.setViewCast} person={c} closeView={this.closeView} updateViewCast={this.updateViewCast} />
+                                        })} </div>) : (<div>
+                                            No available cast
+                                        </div>)}
+    
+                                    </div>
+                                </ProductionList>
                             </div>
-                            </LeftMovieDetails>
-
-                            <RightMovieDetails className='subView'>
-                                <button className='fa fa-close' onClick={this.normalView} style={{ float: 'right' }}></button>
-
-                                <div>
-                                    <BoxDetails> Release date - {this.state.movie.release_date} <br /></BoxDetails>
-
-                                    <BoxDetails>Revenue - ${this.state.movie.revenue} <br /></BoxDetails>
-                                    <BoxDetails> Runtime - {this.state.movie.runtime}m <br /></BoxDetails>
-                                    <BoxDetails> Tagling - {this.state.movie.tagline} <br /></BoxDetails>
-                                    <BoxDetails> <a href={"https://www.themoviedb.org/movie/" + this.state.movie.tmdb_id}> TMDB LINK</a> <br /></BoxDetails>
-                                    <BoxDetails> <a href={"https://www.imdb.com/title/" + this.state.movie.imdb_id}>IMDB LINK</a> <br /> </BoxDetails>
-                                    <BoxDetails> Overview -{this.state.overview} <br /> </BoxDetails>
-                                    <BoxDetails> Popularity-{this.state.ratings.popularity} <br /></BoxDetails>
-
-
-                                    <BoxDetails> Average- {this.state.ratings.average} <br />
-                                        {/* <StarRatings avg={this.state.avg}/> */}
-                                        {this.state.starArray.map((s, i) => s)}
-
-                                    </BoxDetails>
-                                    <BoxDetails> Count-{this.state.ratings.count} <br /></BoxDetails>
-
-                                    <BoxDetails>Companies - {this.state.companies ? (<div> {this.state.companies.map((c, index) => {
-                                        return c.name
-                                    })} </div>) : (<div>
-                                        Companies are not available
-                                    </div>)}</BoxDetails>
-
-                                    <BoxDetails>Countries - {this.state.countries ? (<div> {this.state.countries.map((c, index) => {
-                                        return c.name
-                                    })} </div>) : (<div>
-                                        Countries are not available
-                                    </div>)}</BoxDetails>
-                                    {/* Countries - {this.state.countries.map((c, index) => {
-                                        return c.name
-                                    })} <br /> */}
-
-                                    <BoxDetails> <h3>Keywords</h3> <br/> {this.state.keywords ? (<div> {this.state.keywords.map((c, index) => {
-                                        return c.name + " "
-                                    })} </div>) : (<div>
-                                        Keywords are not available
-                                    </div>)}</BoxDetails>
-                                    {/* Keywords - {this.state.keywords.map((c, index) => {
-                                        return c.name
-                                    })} <br /> */}
-
-                                    <BoxDetails>Genres - {this.state.genres ? (<div> {this.state.genres.map((c, index) => {
-                                        return c.name + " "
-                                    })} </div>) : (<div>
-                                        Genres are not available
-                                    </div>)}</BoxDetails>
-
-                                    {/* Genres - {this.state.genres.map((c, index) => {
-                                        return ", " + c.name 
-                                    })} <br /> */}
-                                </div>
-
-                            </RightMovieDetails>
-
-                            <ProductionList className='subView'>
-                                <div>
-                                    <h2> Production</h2>
-                                    <u>Cast</u> <br />
-                                    {this.state.cast ? (<div> {this.state.cast.map((c, index) => {
-                                        return <Production key={index} setViewCast={this.setViewCast} person={c} closeView={this.closeView} updateViewCast={this.updateViewCast} />
-                                    })} </div>) : (<div>
-                                        No available cast
-                                    </div>)}
-
-                                </div>
-                            </ProductionList>
-                        </div>
-
-                    )}
-            </div>
-        );
+    
+                        )}
+                </div>
+                )}
+            </div>            
+            );
     }
 }
 
