d027f3de61e4709b2a9595170757160768dc8bca
Sushant-Stha <sshre733@mtroyal.ca>
2020-04-07 11:53:17 -0600

    CORS take 6

--- handler/movieRouter.js
+++ handler/movieRouter.js
@@ -8,7 +8,7 @@ const passport = require('passport');
 const router = express.Router();
 
 //handle request for all movie
-router.get('/movies', helper.ensureAuthenticated, (req,resp) => { 
+router.get('/movies', (req,resp) => { 
     MovieModel.find( {}, (err, data) => {
         if(err){
             resp.json({ "message" : 'Unable to connect to movie'});
@@ -20,7 +20,7 @@ router.get('/movies', helper.ensureAuthenticated, (req,resp) => {
 
 
 //handle single movie by id
-router.get('/movies/:id', helper.ensureAuthenticated, (req,resp) => { 
+router.get('/movies/:id', (req,resp) => { 
     MovieModel.find( {id: req.params.id}, (err, data) => {
         if(err || data.length === 0){
             resp.json({ message : 'No movie found with ID of ' + req.params.id});
@@ -32,7 +32,7 @@ router.get('/movies/:id', helper.ensureAuthenticated, (req,resp) => {
 
 
 //handle brief version of all movies
-router.get('/brief', helper.ensureAuthenticated, (req,resp) => { 
+router.get('/brief', (req,resp) => { 
     BriefModel.find( {}, (err, data) => {
         if(err){
             resp.json({ "message" : 'Unable to connect to movie'});
@@ -43,7 +43,7 @@ router.get('/brief', helper.ensureAuthenticated, (req,resp) => {
 });
 
 //handle all movies with substring
-router.get('/find/title/:substring', helper.ensureAuthenticated, (req, resp) => {
+router.get('/find/title/:substring', (req, resp) => {
     MovieModel.find( {title : new RegExp(req.params.substring, 'i')}, (err, data) =>{
         if(err || data.length === 0){
             resp.json( { "message": 'No movie containing ' + req.params.substring + ' found.'})
@@ -55,7 +55,7 @@ router.get('/find/title/:substring', helper.ensureAuthenticated, (req, resp) => 
 
 
 //handle all movies with year in between
-router.get('/find/year/:start/:end' , helper.ensureAuthenticated, (req, resp) => {
+router.get('/find/year/:start/:end', (req, resp) => {
     MovieModel.find().where('release_date')
         .gt(req.params.start)
         .lt(req.params.end)
@@ -69,7 +69,7 @@ router.get('/find/year/:start/:end' , helper.ensureAuthenticated, (req, resp) =>
 });
 
 //handle all movies with rating in between
-router.get('/find/rating/:min/:max' , helper.ensureAuthenticated, (req, resp) => {
+router.get('/find/rating/:min/:max', (req, resp) => {
     MovieModel.find().where('ratings.average')
         .gt(req.params.min)
         .lt(req.params.max)
@@ -83,12 +83,12 @@ router.get('/find/rating/:min/:max' , helper.ensureAuthenticated, (req, resp) =>
 });
 
 //handle get all favorite
-router.get('/favorites', helper.ensureAuthenticated, (req,resp) => {
+router.get('/favorites', (req,resp) => {
     resp.json(req.user.favorites);
 });
 
 //add new movies to users fav list
-router.post('/favorites/:id', helper.ensureAuthenticated, (req,resp) => {
+router.post('/favorites/:id', (req,resp) => {
     //handle single movie by id
     MovieModel.find( {id: req.params.id}, (err, data) => {
         if(err || data.length === 0){
@@ -100,7 +100,7 @@ router.post('/favorites/:id', helper.ensureAuthenticated, (req,resp) => {
 });
 
 //delete movie from users fav list
-router.delete('/favorites/:id', helper.ensureAuthenticated, (req,resp) => {    
+router.delete('/favorites/:id', (req,resp) => {    
     //delete the movie from array
     var temp = {...user.favorites};
     user.favorites = temp.filter( (m) => {return m.id != req.params.id});        
